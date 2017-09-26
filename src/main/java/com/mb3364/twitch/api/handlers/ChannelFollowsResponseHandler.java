b75b50ba9ccba3f5728d122ac4b328931b79e663
Maxim Kulikov <info@maximkulikov.ru>
2017-09-26 21:50:29 +0300

    Fix Mess with Follower model

--- src/main/java/com/mb3364/twitch/api/handlers/ChannelFollowsResponseHandler.java
+++ src/main/java/com/mb3364/twitch/api/handlers/ChannelFollowsResponseHandler.java
@@ -1,9 +1,8 @@
 package com.mb3364.twitch.api.handlers;
 
-import com.mb3364.twitch.api.models.Follow;
-
 import java.util.List;
+import com.mb3364.twitch.api.models.FollowUsers;
 
 public interface ChannelFollowsResponseHandler extends BaseFailureHandler {
-    void onSuccess(int total, List<Follow> follows, String cursor);
+    void onSuccess(int total, List<FollowUsers> follows, String cursor);
 }
