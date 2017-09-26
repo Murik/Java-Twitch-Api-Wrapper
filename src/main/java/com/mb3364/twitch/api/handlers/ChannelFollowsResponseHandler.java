9fcacd78ee549acf2a05a358c1c00cc3e66ef553
Maxim <maksim70@yandex.ru>
2017-09-26 17:21:15 +0300

    Update ChannelFollowsResponseHandler.java
    Fix interface to match method at Update ChannelsResource.java

--- src/main/java/com/mb3364/twitch/api/handlers/ChannelFollowsResponseHandler.java
+++ src/main/java/com/mb3364/twitch/api/handlers/ChannelFollowsResponseHandler.java
@@ -5,5 +5,5 @@ import com.mb3364.twitch.api.models.Follow;
 import java.util.List;
 
 public interface ChannelFollowsResponseHandler extends BaseFailureHandler {
-    void onSuccess(int total, List<Follow> follows);
+    void onSuccess(int total, List<Follow> follows, String cursor);
 }
